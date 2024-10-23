f71c22a7db85e6c982bfacd21ea6727bf7dade88
Codey Gallup <codey.gallup@gmail.com>
2024-10-23 08:33:55 -0600

    Adding mouseover events and random lights puzzle

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 let lightbulbs = document.querySelectorAll(".lightbulb");
+let subtitle = document.querySelector(".subtitle");
+let count = 0;
+
 lightbulbs.forEach((lightbulb) => {
   lightbulb.addEventListener("click", () => {
     lightbulb.classList.toggle("active");
+    count++;
+    subtitle.innerHTML = `You've clicked the lights ${
+      count == 1 ? "once" : count > 1 ? `${count} times` : ""
+    }`;
+    changeRandomLight();
+  });
+  lightbulb.addEventListener("mouseenter", () => {
+    lightbulb.classList.toggle("active");
+  });
+  lightbulb.addEventListener("mouseleave", () => {
+    lightbulb.classList.toggle("active");
   });
 });
+
+const changeRandomLight = () => {
+  for (let i = 0; i < 3; i++) {
+    let randomIndex = Math.floor(Math.random() * lightbulbs.length) + 1;
+    let randomLight = document.querySelector(`#lightbulb${randomIndex}`);
+    randomLight.classList.toggle("active");
+  }
+};
